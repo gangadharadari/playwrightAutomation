0d00641742e7b03247199e4029c85489474262cb
gangadharadari <your-github-gangadharadari18@gmail.com>
2026-09-10 13:26:01 +0530

    yml file updated

--- tests/Admin/job/dd_addjobtitle.spec.js
+++ tests/Admin/job/dd_addjobtitle.spec.js
@@ -2,16 +2,31 @@ import { test, expect } from '@playwright/test';
 
 import logindata from "../../../testdata/login.json"
 
-import jobdata from "../../../testdata/addjobtitle.json"
+// import jobdata from "../../../testdata/addjobtitle.json"
 import { faker } from '@faker-js/faker';
 
 const jobtitles = {
 
-    title1 : faker.company.name(),
-    title2 : faker.company.name(),
-    title3 : faker.company.name(),
-    title4 : faker.company.name(),
-    title5 : faker.company.name()
+    title1 : { name: faker.company.name(),
+               description : faker.lorem.paragraph(),
+               notes : faker.lorem.paragraph()
+             },
+    title2 : { name: faker.company.name(),
+               description : faker.lorem.paragraph(),
+               notes : faker.lorem.paragraph()
+             },
+    title3 : { name: faker.company.name(),
+               description : faker.lorem.paragraph(),
+               notes : faker.lorem.paragraph()
+             },
+    title4 : { name: faker.company.name(),
+               description : faker.lorem.paragraph(),
+               notes : faker.lorem.paragraph()
+             },
+    title5 : { name: faker.company.name(),
+               description : faker.lorem.paragraph(),
+               notes : faker.lorem.paragraph()
+             }
 
 }
 
@@ -35,9 +50,16 @@ test(`Verify Job Title - ${title}`, async ({page}) => {
     await page.getByRole('menuitem', { name: 'Job Titles' }).click();
     await page.getByRole('button', { name: 'Add' }).click();
     await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/saveJobTitle");
-    await page.locator('xpath=(//input[@class=\'oxd-input oxd-input--active\'])[2]').fill(jobtitles[title]);
-    await page.locator('xpath=(//textarea[contains(@class,\'oxd-textarea oxd-textarea--active\')])[1]').fill(jobdata.jobdescription);
-    await page.locator('textarea[placeholder=\'Add note\']').fill(jobdata.notes);
+
+    await page.locator('xpath=(//input[@class=\'oxd-input oxd-input--active\'])[2]').fill(jobtitles[title].name);
+    console.log("JOB TITLE:", jobtitles[title].name);
+
+    await page.locator('xpath=(//textarea[contains(@class,\'oxd-textarea oxd-textarea--active\')])[1]').fill(jobtitles[title].description);
+    console.log("DESCRIPTION:", jobtitles[title].description);
+
+    await page.locator('textarea[placeholder=\'Add note\']').fill(jobtitles[title].notes);
+    console.log("NOTES:", jobtitles[title].notes);
+
     await page.locator('//button[@type=\'submit\']').click();
     await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/admin/viewJobTitleList");
     await page.close();
